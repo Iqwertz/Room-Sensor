ff43adebd708d1bc488bac27b202fddb44800338
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2022-12-08 20:30:01 +0100

    Delete credentials.h

--- RoomSensor/credentials.h
+++
@@ -1,4 +0,0 @@
-const char* ssid = "your-wifi-ssid";
-const char* password = "your-wifi-password";
-const char* FIREBASE_HOST = "your-firebase-host.firebaseio.com";
-const char* FIREBASE_AUTH = "your-firebase-auth-key";
