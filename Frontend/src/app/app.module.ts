a06636a4a27de0e9b0dfec3e2fce2c2e3f1c10ac
Iqwertz <36999445+Iqwertz@users.noreply.github.com>
2022-12-09 19:00:45 +0100

    firebase ini

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { firebaseConfig } from '../environments/firebaseConfig';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
